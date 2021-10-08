7f977e7daa97f9161bc9d6ac4f2f0ad724021506
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-08 12:35:56 +0300

    added bootsrap 5

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picha',
+        'USER': 'moringaaccess',
+    'PASSWORD':'access',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa,Nairobi'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
