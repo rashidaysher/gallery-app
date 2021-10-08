a96cb7cf95839576ec086e799273ad2038b7567f
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-08 12:43:01 +0300

    added the category model

--- picha/models.py
+++ picha/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#category class
+class Category(models.Model):
+    name = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.name
