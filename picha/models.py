5ea96feb13fc0dedbbec21e4c35aaa355d2857c9
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-08 21:56:52 +0300

    added migrations

--- picha/models.py
+++ picha/models.py
@@ -62,8 +62,8 @@ class Image(models.Model):
     picha = models.ImageField(upload_to = 'uploads/')
     pic = ImageField(blank = True, manual_crop= '1950x2000')
     description = models.TextField()
-    image_location = models.ForeignKey('Location',on_delete=models.CASCADE,)
-    image_category = models.ForeignKey('Category',on_delete=models.CASCADE,)
+    image_location = models.ForeignKey('Location',on_delete=models.CASCADE,default='')
+    image_category = models.ForeignKey('Category',on_delete=models.CASCADE,default='')
 
     def save_image(self):
         self.save()
@@ -73,7 +73,7 @@ class Image(models.Model):
 
     @classmethod
     def update_image(cls, id, name, description, image_location, image_category):
-        update = cls.objects.filter(id = id).update(name= name, description = description, image_location = image_location, image= image_category)  
+        update = cls.objects.filter(id = id).update(name= name, description = description, image_location = image_location, image_category= image_category)  
 
     @classmethod
     def get_all_images(cls):
