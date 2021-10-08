03b86f9e9b288ca3688b7f7924d0765209bdfb31
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-08 22:09:01 +0300

    defined the image view fuction

--- picha/views.py
+++ picha/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from re import L
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse, Http404
+from .models import Image, Category, Location
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the kiliga photography')
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'Kiliga_photography'
+
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
