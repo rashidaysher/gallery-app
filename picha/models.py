87ebea842d5a5415a4839895a1c2caefd59b1987
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-08 19:25:13 +0300

    added the save, delete and update of location model

--- picha/models.py
+++ picha/models.py
@@ -30,6 +30,22 @@ class Category(models.Model):
     # location class
 
 class Location(models.Model):
-    
+    name = models.CharField(max_length= 40)
+
+    @classmethod
+    def tag_articles(cls):
+        tags= cls.objects.all()
+        return tags
               
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
+        
 
