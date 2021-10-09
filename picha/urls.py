b6ed3423aa8c5985f6a9836801c73a460361eb89
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-09 13:50:59 +0300

    styling the navbar

--- picha/urls.py
+++ picha/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     path('',views.index,name = 'index'),
     path('search/', views.search_image, name='search_image'),
     path('location/image_location', views.location_filter, name='location_filter'),
-    path('image/category_name/<image_id>',views.single,name = 'single')
+    path('image/category_name/image_id',views.single,name = 'single')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
