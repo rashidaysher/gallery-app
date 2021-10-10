cca16dc89317a99e0aeb464e91d710269722825b
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-11 00:32:01 +0300

    added tests of location

--- picha/tests.py
+++ picha/tests.py
@@ -14,45 +14,45 @@ class LocationTestCase(TestCase):
         
 class CategoryTestCase(TestCase):
     def setUp(self):
-        Category.objects.create(name='Car')
-        self.name = Category(name='Car')
+        Category.objects.create(name='Food')
+        self.name = Category(name='Food')
         
     def test_instance(self):
         self.assertTrue(isinstance(self.name,Category))
     
 class TestImage(TestCase):
     def setUp(self):
-        self.location = Location(name='Moringa')
+        self.location = Location(name='Nairobi')
         self.location.save()
 
-        self.category = Category(name='home')
-        self.category.save()
+        self.pic_category = Category(name='home')
+        self.pic_category.save()
 
-        self.image_test = Image(id=1, image='image.jpg', description='this is a test image', location=self.location,
-                                category=self.category)
+        self.picture_test = Image(id=1, picture='picture.jpg', description='this is a test picture', location=self.location,
+                                pic_category=self.pic_category)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.image_test, Image))
+        self.assertTrue(isinstance(self.picture_test, Image))
 
     def tearDown(self):
         Image.objects.all().delete()
         Location.objects.all().delete()
         Category.objects.all().delete()
 
-    def test_update_image(self):
-        self.image_test.save_image()
-        self.image_test.update_image(self.image_test.id, 'photos/test.jpg')
-        changed_img = Image.objects.filter(image='photos/test.jpg')
+    def test_update_picture(self):
+        self.picture_test.save_image()
+        self.picture_test.update_image(self.picture_test.id, 'picha/test.jpg')
+        changed_img = Image.objects.filter(image='picha/test.jpg')
         self.assertTrue(len(changed_img) > 0)
 
 
     def test_save_image(self):
-        self.image_test.save_image()
+        self.picture_test.save_image()
         after = Image.objects.all()
         self.assertTrue(len(after) > 0)
 
     def test_delete_image(self):
-        self.image_test.delete_image()
+        self.picture_test.delete_image()
         images = Image.objects.all()
         self.assertTrue(len(images) == 0)
 
